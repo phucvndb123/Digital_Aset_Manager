9a397fa426a2bcadbf03d89e58322f0de2f35e2d
tientranminhforwork <tientranminh.forwork@gmail.com>
2026-05-11 17:29:43 +0700

    fix sidebar logic and add layout of assestdetail

--- WebContent/websquare/app.js
+++ WebContent/websquare/app.js
@@ -16,6 +16,19 @@
             } catch (err) {}
         }
 
+        await Promise.all([
+            WebSquare._loadModuleDynamic("uiplugin.wframe"),
+            WebSquare._loadModuleDynamic("uiplugin.tabControl"),
+            WebSquare._loadModuleDynamic("uiplugin.textbox"),
+            WebSquare._loadModuleDynamic("uiplugin.trigger"),
+            WebSquare._loadModuleDynamic("uiplugin.group"),
+            WebSquare._loadModuleDynamic("uiplugin.image"),
+            WebSquare._loadModuleDynamic("uiplugin.gridView"),
+            WebSquare._loadModuleDynamic("uiplugin.treeview"),
+            WebSquare._loadModuleDynamic("uiplugin.input"),
+            WebSquare._loadModuleDynamic("uiplugin.selectbox")
+        ]);
+
         if (w2xPath) {
             return WebSquare.startApplication(w2xPath);
         }
